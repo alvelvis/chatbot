7e40987c58c8408878635b615f951100d95a0fe1
Elvis de Souza <elvis.desouza99@gmail.com>
2021-05-03 02:54:12 -0300

    first prototype

---
+++ flask/app.py
@@ -0,0 +1,147 @@
+from flask import Flask, render_template, jsonify, request
+import os
+import sys
+import shutil
+import re
+import json
+import urllib
+import estrutura_ud
+import interrogar_UD
+import numpy as np
+from gensim.test.utils import datapath, get_tmpfile
+from gensim.models import Word2Vec
+import Levenshtein
+import pprint
+import unidecode
+import random
+import wikipedia
+from bs4 import BeautifulSoup
+wikipedia.set_lang("pt")
+
+app = Flask(__name__)
+
+def remove_accents(word):
+    return unidecode.unidecode(word)
+
+model = Word2Vec.load("Publico-Hibrido(Completo+NILC).txt.model")
+hybrid = model.wv
+
+contrast_corpus = "bosque-ud-2.6.conllu"
+with open(contrast_corpus) as f:
+    contrast_corpus = list(map(lambda y: y.split("\t")[2].lower(), list(filter(lambda x: len(x.split("\t")) == 10 and x.split("\t")[3] in ["NOUN", "VERB", "PROPN"], f.read().splitlines()))))
+frequency_of_important_words = {x: contrast_corpus.count(x) for x in set(contrast_corpus)}
+
+linguistic_patterns = {
+    'functions': [
+        [
+            {
+                'lemma': 'você|tu|qual|quais',
+                'deprel': 'nsubj',
+                'head_token.lemma': 'fazer|realizar|função',
+            }
+        ],
+        [
+        
+            {
+                'lemma': 'você|tu|qual|quais',
+                'deprel': 'nsubj',
+            },
+            {
+                'lemma': 'fazer|realizar|função',
+                'deprel': 'xcomp',
+            }
+        ]
+    ]
+}
+
+# EXTEND LINGUISTIC PATTERNS WITH SIMILAR WORDS
+for resp, pattern in linguistic_patterns.items():
+    for q, query in enumerate(pattern):
+        for e, exp in enumerate(query):
+            expression = []
+            for col in exp:
+                if 'lemma' in col:
+                    linguistic_patterns[resp][q][e][col] = remove_accents(linguistic_patterns[resp][q][e][col]).split("|")
+                    if any(x in hybrid.key_to_index for x in linguistic_patterns[resp][q][e][col]):
+                        linguistic_patterns[resp][q][e][col].extend(list(map(lambda x: x[0], hybrid.most_similar(positive=list(filter(lambda x: x in hybrid.key_to_index, linguistic_patterns[resp][q][e][col])), topn=10))))
+                    linguistic_patterns[resp][q][e][col] = "|".join(list(set(linguistic_patterns[resp][q][e][col])))
+                expression.append('{} = "{}"'.format(col, linguistic_patterns[resp][q][e][col]))
+            linguistic_patterns[resp][q][e] = " and ".join(expression)  
+print(json.dumps(linguistic_patterns, indent=4, ensure_ascii=False))
+
+responses = {
+    'functions': "Ainda não tenho uma lista de funcionalidades, me desculpe..."
+}
+
+app_dict = {
+    'keywords': [
+        ("tudo o que eu faço", "O que você pode fazer?")
+    ],
+}
+
+@app.route('/', methods=["POST"])
+def api():
+    api_response = ""
+    bot_response = ""    
+   
+    # PARSE THE USER INPUT AND LOOK FOR PATTERNS
+    if request.form.get('api_response'):
+        api_response = app_dict[request.form.get('api_response')]
+    if request.form.get('input'):
+        udpipe_url = "http://lindat.mff.cuni.cz/services/udpipe/api/process?tokenizer&tagger&parser"
+        udpipe_data = urllib.parse.urlencode({
+            'data': request.form.get('input'),
+            'model': "portuguese-bosque-ud-2.6-200830",
+            }).encode('ascii')
+        with urllib.request.urlopen(udpipe_url, udpipe_data) as f:
+            udpipe_output = json.loads(remove_accents(f.read().decode('utf-8')))['result']
+            text = estrutura_ud.Corpus(recursivo=True)
+            text.build(udpipe_output)
+        print("input: {}".format(text.to_str()))
+
+        # try to find linguistic pattern   
+        for pattern in linguistic_patterns:
+            for query in linguistic_patterns[pattern]:
+                #print(query)
+                if all(interrogar_UD.main(text, 5, x, fastSearch=True)['casos'] for x in query):
+                    bot_response = responses[pattern]
+                    break
+        
+        # try to answer from wikipedia
+        if not bot_response:
+            # get most awkward word in input
+            names = []
+            verbs = []
+            for sentence in text.sentences.values():
+                for token in sentence.tokens:
+                    clean_token = token.lemma.lower()
+                    if token.upos in ["NOUN", "PROPN"] and not clean_token in names and clean_token in frequency_of_important_words:
+                        names.append(clean_token)
+                    if token.upos in ["VERB"] and not clean_token in verbs and clean_token in frequency_of_important_words:
+                        verbs.append(clean_token)
+            most_awkward_name = sorted(names, key=lambda x: frequency_of_important_words[x])
+            most_awkward_verb = sorted(verbs, key=lambda x: frequency_of_important_words[x])
+            if most_awkward_name:
+                try:
+                    bot_response = wikipedia.summary(most_awkward_name[0], sentences=2)
+                except wikipedia.DisambiguationError as e:
+                    s = random.choice(e.options)
+                    bot_response = wikipedia.summary(s, sentences=2)
+        
+        # try to answer from "pensador"
+        if not bot_response and most_awkward_verb:
+            print("https://www.pensador.com/{}/".format(most_awkward_verb[0]))
+            with urllib.request.urlopen("https://www.pensador.com/{}/".format(most_awkward_verb[0])) as f:
+                #html = f.read().decode('utf-8')
+                soup = BeautifulSoup(f, "html.parser")
+                bot_response = random.choice(soup.find_all("p", class_="frase")).get_text()
+        
+        # no answer found
+        if not bot_response:
+            bot_response = "Desculpe, ainda não sei como responder..."
+
+    response = jsonify({"api_response": api_response, "bot_response": bot_response})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+app.run(debug=True)
--- index.js
+++ index.js
@@ -1,97 +1,87 @@
+var timeout = 0;
+var delay = 500;
+var keywords = [];
+var typing = false;
+flask_http = (window.location.href.match(/127\.0\.0\.1/g)) ? "http://127.0.0.1:5000" : "https://alvelvis-chatbot.herokuapp.com";
+
 document.addEventListener("DOMContentLoaded", () => {
   const inputField = document.getElementById("input");
   inputField.addEventListener("keydown", (e) => {
-    if (e.code === "Enter") {
+    if (e.keyCode == 13 && inputField.value.trim().length > 0) {
       let input = inputField.value;
       inputField.value = "";
       output(input);
     }
   });
+  addChat("", "Olá! Me pergunte sobre algo que eu respondo.");
+  addChat("", "Quer saber tudo o que eu faço?");
+
+  $.post(flask_http, {api_response: "keywords"}, function(data){
+    keywords = data.api_response;
+  });
+
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const q = urlParams.get('q')
+  if (q) { input.value = decodeURI(q) };  
+
 });
 
 function output(input) {
-  let product;
-
-  // Regex remove non word/space chars
-  // Trim trailing whitespce
-  // Remove digits - not sure if this is best
-  // But solves problem of entering something like 'hi1'
-
-  let text = input.toLowerCase().replace(/[^\w\s]/gi, "").replace(/[\d]/gi, "").trim();
-  text = text
-    .replace(/ a /g, " ")   // 'tell me a story' -> 'tell me story'
-    .replace(/i feel /g, "")
-    .replace(/whats/g, "what is")
-    .replace(/please /g, "")
-    .replace(/ please/g, "")
-    .replace(/r u/g, "are you");
-
-  if (compare(prompts, replies, text)) { 
-    // Search for exact match in `prompts`
-    product = compare(prompts, replies, text);
-  } else if (text.match(/thank/gi)) {
-    product = "You're welcome!"
-  } else if (text.match(/(corona|covid|virus)/gi)) {
-    // If no match, check if message contains `coronavirus`
-    product = coronavirus[Math.floor(Math.random() * coronavirus.length)];
-  } else {
-    // If all else fails: random alternative
-    product = alternative[Math.floor(Math.random() * alternative.length)];
-  }
-
-  // Update DOM
-  addChat(input, product);
-}
-
-function compare(promptsArray, repliesArray, string) {
-  let reply;
-  let replyFound = false;
-  for (let x = 0; x < promptsArray.length; x++) {
-    for (let y = 0; y < promptsArray[x].length; y++) {
-      if (promptsArray[x][y] === string) {
-        let replies = repliesArray[x];
-        reply = replies[Math.floor(Math.random() * replies.length)];
-        replyFound = true;
-        // Stop inner loop when input value matches prompts
-        break;
-      }
-    }
-    if (replyFound) {
-      // Stop outer loop when reply is found instead of interating through the entire array
-      break;
-    }
-  }
-  return reply;
+  addChat(input, "");
+  window.history.pushState({}, "", window.location.href.split("/chatbot")[0] + "/chatbot/?q=" + encodeURI(input));
 }
 
 function addChat(input, product) {
   const messagesContainer = document.getElementById("messages");
+    
+  if (input.trim().length > 0) {
+    let userDiv = document.createElement("div");
+    userDiv.id = "user";
+    userDiv.className = "user response";
+    userDiv.innerHTML = `<img src="user.png" class="avatar"><span>${input}</span>`;
+    messagesContainer.appendChild(userDiv);
+    messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
+  };  
 
-  let userDiv = document.createElement("div");
-  userDiv.id = "user";
-  userDiv.className = "user response";
-  userDiv.innerHTML = `<img src="user.png" class="avatar"><span>${input}</span>`;
-  messagesContainer.appendChild(userDiv);
-
-  let botDiv = document.createElement("div");
-  let botImg = document.createElement("img");
-  let botText = document.createElement("span");
-  botDiv.id = "bot";
-  botImg.src = "bot-mini.png";
-  botImg.className = "avatar";
-  botDiv.className = "bot response";
-  botText.innerText = "Typing...";
-  botDiv.appendChild(botText);
-  botDiv.appendChild(botImg);
-  messagesContainer.appendChild(botDiv);
-  // Keep messages at most recent
-  messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
-
-  // Fake delay to seem "real"
-  setTimeout(() => {
-    botText.innerText = `${product}`;
-    textToSpeech(product)
-  }, 2000
-  )
-
+  $.post(flask_http, {'input': product.length > 0 ? "" : input}, function(data){
+    timeout = timeout + delay;
+    setTimeout(() => {
+      typing = true;
+      let botDiv = document.createElement("div");
+      let botImg = document.createElement("img");
+      let botText = document.createElement("span");
+      botDiv.id = "bot";
+      botImg.src = "bot-mini.png";
+      botImg.className = "avatar";
+      botDiv.className = "bot response";
+      botText.innerText = "Digitando...";
+      botDiv.appendChild(botText);
+      botDiv.appendChild(botImg);
+      messagesContainer.appendChild(botDiv);
+      // Keep messages at most recent
+      messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
+           
+      product = product.length > 0 ? product : data.bot_response;
+      
+      // Fake delay to seem "real"
+      setTimeout(() => {
+        timeout = timeout - delay
+        botText.innerText = `${product}`;
+        for (kw of keywords) {
+          if (botText.innerText.toLowerCase().indexOf(kw[0].toLowerCase()) != -1) {
+            botText.innerHTML = botText.innerHTML.replace(kw[0], `<a reply="${kw[1]}" class="write-this">${kw[0]}</a>`)
+          }
+        }
+        $('.write-this').unbind('click').click(function() { writeThis($(this).attr('reply')) });
+        textToSpeech(product);
+        typing = false;
+      }, delay);
+      
+    }, timeout);
+  });
 }
+
+function writeThis(data) {
+  output(data);
+};
